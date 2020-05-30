4339980ec03229a2d8646bcef62c22c253c86395
Markimus <Markzero948@aol.com>
2020-05-30 11:50:09 +0100

    Toggle on spacebar added

---
+++ Parallel Worlds/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParallelWorlds
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        [SerializeField] private GameEvent switchEvent = null;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                switchEvent.Raise();
+            }
+        }
+    }
+}
