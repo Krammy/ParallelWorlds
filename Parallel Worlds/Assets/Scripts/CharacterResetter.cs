b8948ac8a6d4ada098d9b6ee638d71a12cf8d0a8
Markimus <Markzero948@aol.com>
2020-05-30 19:10:56 +0100

    Level Resetting

---
+++ Parallel Worlds/Assets/Scripts/CharacterResetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ParallelWorlds
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class CharacterResetter : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent afterReset = null;
+
+        private Rigidbody2D rb2d = null;
+
+        private void Awake()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        public void ResetCharacter()
+        {
+            rb2d.velocity = Vector2.zero;
+            transform.localPosition = Vector3.zero;
+
+            afterReset.Invoke();
+        }
+    }
+}
---
+++ Parallel Worlds/Assets/Scripts/OutOfBoundsChecker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ParallelWorlds
+{
+    /// <summary>
+    /// Checks if this object is out of bounds.
+    /// </summary>
+    public class OutOfBoundsChecker : MonoBehaviour
+    {
+        [SerializeField] private float boundsPosition = -10f;
+        [SerializeField] private UnityEvent onOutOfBounds = null;
+
+        private bool debounce = false;
+
+        private void Update()
+        {
+            if (debounce || transform.position.y > boundsPosition) return;
+            debounce = true;
+            onOutOfBounds.Invoke();
+        }
+
+        public void ResetDebounce()
+        {
+            debounce = false;
+        }
+    }
+}
