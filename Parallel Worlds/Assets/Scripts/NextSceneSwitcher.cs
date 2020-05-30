57bfbe6392e19a71c7778509db18a3e839c39293
Markimus <Markzero948@aol.com>
2020-05-30 18:15:50 +0100

    Added gate scene switching

---
+++ Parallel Worlds/Assets/Scripts/NextSceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ParallelWorlds
+{
+    public class NextSceneSwitcher : MonoBehaviour
+    {
+        public void SwitchScene()
+        {
+            int nextLevel = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCount;
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
