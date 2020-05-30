7b589ee6530bc94cb097f12a83be3084d605dacd
Markimus <Markzero948@aol.com>
2020-05-30 16:18:05 +0100

    basic ground checking

--- Parallel Worlds/Assets/Scripts/CharacterMove.cs
+++ Parallel Worlds/Assets/Scripts/CharacterMove.cs
@@ -9,8 +9,11 @@ namespace ParallelWorlds
     {
         [SerializeField] private float speed = 10f;
         [SerializeField] private float jumpForce = 10f;
+        [SerializeField] private float groundTolerance = 0.1f;
 
         private Rigidbody2D rb2d = null;
+
+        private int touchingAmount = 0;
 
         private void Awake()
         {
@@ -30,7 +33,38 @@ namespace ParallelWorlds
 
         private void Jump()
         {
-            rb2d.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+            // only jump if moving down
+            if (rb2d.velocity.y > 0f) return;
+
+            // get contacts
+            List<ContactPoint2D> contactPoints = new List<ContactPoint2D>();
+            rb2d.GetContacts(contactPoints);
+
+            Vector2 myPos = transform.position;
+
+            // check normals of contacts, discarding others
+            foreach (ContactPoint2D contactPoint in contactPoints)
+            {
+                float rot = Vector2.Angle(Vector2.zero, contactPoint.normal);
+                if (rot >= -45f && rot <= 45f)
+                {
+                    // add jump force
+                    rb2d.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+                    return;
+                }
+            }
+
+            // create raycast
+            
+
+            //Vector2 bottomLeft = myPos + new Vector2(-0.45f, -0.5f - groundTolerance);
+            //Vector2 topRight = myPos + Vector2.one * 0.45f;
+            //Vector2 size = topRight - bottomLeft;
+
+            //Collider2D hit = Physics2D.OverlapBox(transform.position, size, 0f);
+            //if (hit == null) return;
+
+            
         }
 
         private void FixedUpdate()
