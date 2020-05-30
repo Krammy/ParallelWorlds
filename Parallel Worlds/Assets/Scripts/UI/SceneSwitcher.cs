972f49ddde9fe6e928920a44f9a3c3b0fca0ba40
Markimus <Markzero948@aol.com>
2020-05-30 16:11:51 +0100

    Added Scene Switcher

---
+++ Parallel Worlds/Assets/Scripts/UI/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ParallelWorlds
+{
+    public class SceneSwitcher : MonoBehaviour
+    {
+        [SerializeField] private int sceneBuildIndex = 0;
+
+        public void SwitchScene()
+        {
+            SceneManager.LoadScene(sceneBuildIndex);
+        }
+    }
+}
