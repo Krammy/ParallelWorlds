af757732760efaea5e9e2ec62a1a9b70dddf19ca
Markimus <Markzero948@aol.com>
2020-05-30 20:57:30 +0100

    preparations for new scene

---
+++ Parallel Worlds/Assets/Scripts/SwitchListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ParallelWorlds
+{
+    public class SwitchListener : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent onSwitch = null;
+
+        private void OnEnable()
+        {
+            PlayerInput.Instance.OnSwitch += OnSwitch;
+        }
+
+        private void OnDisable()
+        {
+            if (PlayerInput.Instance == null) return;
+            PlayerInput.Instance.OnSwitch -= OnSwitch;
+        }
+
+        private void OnSwitch()
+        {
+            onSwitch.Invoke();
+        }
+    }
+}
