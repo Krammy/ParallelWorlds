fe8d06be9223d73147a2c5341e3e590d5e934ba7
Markimus <Markzero948@aol.com>
2020-05-30 13:52:33 +0100

    Added hover title

---
+++ Parallel Worlds/Assets/Scripts/Hover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ParallelWorlds
+{
+    public class Hover : MonoBehaviour
+    {
+        [SerializeField] private float hoverAmount = 1f;
+        [SerializeField] private float hoverTime = 1f;
+
+        private float startY = 0f;
+
+        private void OnEnable()
+        {
+            startY = transform.position.y;
+        }
+
+        private void Update()
+        {
+            float pos1 = startY + hoverAmount;
+            float pos2 = startY - hoverAmount;
+
+            Vector3 newPos = transform.position;
+            newPos.y = Mathf.Lerp(pos1, pos2, Mathf.Sin(Mathf.PI * (Time.time / hoverTime))/2f + 0.5f);
+
+            transform.position = newPos;
+        }
+    }
+}
