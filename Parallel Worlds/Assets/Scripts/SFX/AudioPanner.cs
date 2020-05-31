fb7d1eb532b3bc62720ea2dbb580e945ce843e8f
Markimus <Markzero948@aol.com>
2020-05-31 19:42:14 +0100

    added audio panning

---
+++ Parallel Worlds/Assets/Scripts/SFX/AudioPanner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParallelWorlds
+{
+    /// <summary>
+    /// pans audiosources left/right depending on location
+    /// </summary>
+    public class AudioPanner : MonoBehaviour
+    {
+        [SerializeField] private AudioSource[] audioSources = null;
+
+        private const float minX = -8.8f;
+        private const float maxX = 8.8f;
+        private const float panAmount = 0.5f;
+
+        private void Update()
+        {
+            // -8.8 to 8.8
+            float posX = transform.position.x;
+            float lerpAmount = (posX - minX) / (maxX - minX) * 2f - 0.5f;
+            lerpAmount *= panAmount;
+
+            foreach(AudioSource audioSource in audioSources)
+            {
+                audioSource.panStereo = lerpAmount;
+            }
+        }
+    }
+}
