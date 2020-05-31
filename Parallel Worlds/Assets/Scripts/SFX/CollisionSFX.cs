b88966c937b132d2b6a8785ef6b1eb3bf2e553ff
Markimus <Markzero948@aol.com>
2020-05-31 12:25:56 +0100

    added collision SFX

---
+++ Parallel Worlds/Assets/Scripts/SFX/CollisionSFX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParallelWorlds
+{
+    public class CollisionSFX : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            float multiplier = collision.relativeVelocity.magnitude;
+            audioSource.PlayOneShot(audioSource.clip, audioSource.volume * multiplier);
+        }
+    }
+}
