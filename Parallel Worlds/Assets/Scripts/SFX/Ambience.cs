88776406c2616fdfbf53da3af8d633b7ff59e2b6
Markimus <Markzero948@aol.com>
2020-05-31 11:57:37 +0100

    added basic ambience

---
+++ Parallel Worlds/Assets/Scripts/SFX/Ambience.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParallelWorlds
+{
+    public class Ambience : MonoBehaviour
+    {
+        [Tooltip("The audio fade after each ambience track.")]
+        [SerializeField] private float rolloffSeconds = 0.5f;
+        [SerializeField] private AudioSource[] audioSources = null;
+        private int playingIndex = 0;
+
+        public void Switch()
+        {
+            // fade out one audio source, fade in other
+            StartCoroutine(FadeOutAudioSource(audioSources[playingIndex]));
+            playingIndex = (playingIndex + 1) % audioSources.Length;
+
+            audioSources[playingIndex].volume = 1f;
+            audioSources[playingIndex].Play();
+            Debug.Log("Playing AudioSource #" + playingIndex);
+        }
+
+        private IEnumerator FadeOutAudioSource(AudioSource audioSource)
+        {
+            while (audioSource.volume > 0f)
+            {
+                audioSource.volume = Mathf.Max(audioSource.volume - Time.deltaTime / rolloffSeconds, 0f);
+                yield return 0f;
+            }
+            audioSource.volume = 0f;
+            audioSource.Stop();
+            Debug.Log("Completed fade out.");
+        }
+    }
+}
